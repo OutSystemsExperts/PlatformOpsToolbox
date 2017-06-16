a90a045b54b2f92c791ffe4aa420c6e75ae559c9
Paulo Costa <pauloalexcosta@users.noreply.github.com>
2017-06-16 10:56:56 +0100

    Update OutSystemsInstaller.sh

--- AutomaticInstaller/Linux/OutSystemsInstaller.sh
+++ AutomaticInstaller/Linux/OutSystemsInstaller.sh
@@ -141,7 +141,7 @@ then
 	echo "jdk-8u121-linux-x64.rpm exists in folder, will not re-download."
 else
 	echo "I will now download jdk-8u121-linux-x64.rpm"
-	wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
+	wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 	returncode "Download JDK from Oracle"
 fi
 rpm -i jdk-8u121-linux-x64.rpm ;
